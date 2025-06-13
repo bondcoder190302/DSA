9cf4f9e3ab67c8406afbb4b4ebbca094cf8e12cd
Vipul Tiwari <vipultiwari190302@gmail.com>
2025-06-13 05:44:39 +0530

    Update BubbleSortRecursion.java

--- BubbleSortRecursion.java
+++ BubbleSortRecursion.java
@@ -1,13 +1,19 @@
 public class BubbleSortRecursion {
     public static int[] bubbleSort(int[] nums, int low, int high){
+        int didswap;
         if(high>0){ 
+            didswap=0;
             for(int i = 0; i<high;i++){
                 if(nums[i]>nums[i+1]){
                     int temp=nums[i+1];
                     nums[i+1]=nums[i];
                     nums[i]=temp;
+                    didswap+=1;
                 }
             }
+            if(didswap==0){
+                System.out.println("No swap done");
+                return nums;}
             bubbleSort(nums, 0, high-1);
             return nums;
         }
