76040b6386209c0b454b04008932892db736e247
Vipul Tiwari <vipultiwari190302@gmail.com>
2025-07-09 03:53:07 +0530

    Add files via upload

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,16 +1,25 @@
 public class MissingNumber {
     public static int missing(int n, int[] nums){
-        int i=1;
-        while(i<=n){
-            if(i==nums[i-1]){
-                i++;
-                continue;
-            }
-            else{
-                return i;
-            }
+        //====== CODE-1 ======
+        //int i=1;
+        //while(i<=n){
+        //    if(i==nums[i-1]){
+        //        i++;
+        //        continue;
+        //    }
+        //    else{
+        //        return i;
+        //    }
+        //}
+        //return -1;
+        //====== CODE-2 ======
+        int xor1=0, xor2=0;
+        for(int i=0; i<n-1;i++){
+            xor1=xor1^(i+1);
+            xor2=xor2^nums[i];
         }
-        return i;
+        xor1 = xor1^n;
+        return xor1^xor2;
     } 
     public static void main(String[] args){
         int N=5;
